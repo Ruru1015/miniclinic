cc7c359e535348ee2e537b9f9dd0ef2976bd4b17
Ruru1015 <414571051@m365.fju.edu.tw>
2026-06-03 16:41:24 +0800

    新增取消和看診完成

--- src/main/java/tw/edu/fju/miniclinic/model/AppointmentRepository.java
+++ src/main/java/tw/edu/fju/miniclinic/model/AppointmentRepository.java
@@ -5,13 +5,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-
     List<Appointment> findByApptDate(LocalDate apptDate);
     List<Appointment> findByDoctor(Doctor doctor);
     List<Appointment> findByPatient(Patient patient);
-    long countByApptDateBetween(LocalDate from, LocalDate to);
-    long countByDoctor_Department(String department);// 根據科別統計預約數量
-    List<Appointment> findByDoctorAndApptDate(Doctor doctor, LocalDate apptDate);  // 新加入
-
-    long countByStatus(String status); // 根據狀態統計數量
+    List<Appointment> findByDoctorAndApptDate(Doctor doctor, LocalDate apptDate);
+    
+    // 新增：用於統計 API
+    long countByStatus(String status);
 }
